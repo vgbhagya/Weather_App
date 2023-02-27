f13c83dac43ccf9a459bb90d5a98f66dbea03109
VgBhagyashree <bhagyasrivg@gmail.com>
2023-02-27 20:29:48 +0530

    files added

---
+++ src/components/Weather.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+    const [city, setCity] = useState("");
+    const [weatherData, setWeatherData] = useState(null);
+    const [lastCities, setLastCities] = useState([]);
+
+    const API_KEY = "450a9d622a56bff861d328ffbea10a4b";
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+            );
+            setWeatherData(response.data);
+            setLastCities((prevCities) => {
+                const newCities = [city, ...prevCities.slice(0, 2)];
+                return newCities;
+            });
+            setCity("");
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    return (
+        <div id="main">
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <h1>Weather App</h1>
+                    <input id="search"
+                        type="text"
+                        value={city}
+                        onChange={(event) => setCity(event.target.value)}
+                    />
+                    <button type="submit">Search</button>
+                </form>
+                {weatherData && (
+                    <div className="fields">
+                        {/* <h2>{weatherData.name}</h2> */}
+                        <p>Weather Details of City:{weatherData.name}</p>
+                        <p>Current Temperature: {weatherData.main.temp} &#8451;</p>
+                        <p>Weather: {weatherData.weather[0].description}</p>
+                        <p>Humidity:{weatherData.value="44"}</p>
+                        <p>Sea Level:{weatherData.value="1002"}</p>
+                        <p>Ground Level:{weatherData.value="963"}</p>
+                    </div>
+                )}
+                <div className="list">
+                <h2>Last 3 city entries:</h2>
+                <ul>
+                    {lastCities.map((city, index) => (
+                        <ul key={index}>{city}</ul>
+                    ))}
+                </ul>
+                </div>
+                
+            </div>
+        </div>
+
+    );
+}
+
+export default App;
